30600df37bb386301a2d1884ba1b0a0054dd6357
Gabrielle <gaparecidapichau@gmail.com>
2022-06-10 11:23:21 -0300

    salvo

---
+++ src/views/TimesView.vue
@@ -0,0 +1,112 @@
+<script>
+import { v4 as uuidv4 } from "uuid";
+export default {
+  data() {
+    return {
+      times: [
+        { id: "449dd5a3-269d-4cbe-9f3d-e3d39fddcaa5", nome: "Time 1" },
+        { id: "7662314f-0038-4e0c-886a-9a0334bdbab2", nome: "TIme 2" },
+        { id: "a2aa61b1-bc45-41d1-9b21-d84f4b94c3ad", nome: "Time 3" },
+        { id: "a3f33a40-915f-4fa3-8624-13b63ea90a87", nome: "Time 4" },
+      ],
+      novo_time: "",
+    };
+  },
+methods: {
+  salvar() {
+    const novo_id = uuidv4();
+    this.times.push( {
+      id: novo_id,
+      nome: this.novo_time,
+    });
+  }
+}
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="title">
+      <h2>Gerenciamento de Times</h2>
+    </div>
+    <div class="form-input">
+      <input type="text" v-model="novo_time"/>
+      <button @click="salvar">Salvar</button>
+    </div>
+    <div class="list-times">
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="time in times" :key="time.id">
+            <td>{{ time.id }}</td>
+            <td>{{ time.nome }}</td>
+            <td>_________</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <RouterView />
+</template>
+
+<style>
+.title {
+  text-align: center;
+  margin: 2rem 0;
+}
+
+.form-input {
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0;
+}
+
+.form-input input {
+  width: 50%;
+  padding: 0.5rem;
+  border: 5px solid #98b7e1;
+  border-radius: 10px;
+}
+
+.form-input button {
+  padding: 0.5rem;
+  border: 5px solid #98b7e1;
+  border-radius: 10px;
+  color: #454663;
+  font-weight: bold;
+  margin-left: 1%;
+}
+
+.list-times {
+  display: flex;
+  justify-content: center;
+}
+
+table {
+  width: 80%;
+  border-collapse: collapse;
+  margin: 0 auto;
+  border: 1px solid #9bfae2;
+  font-size: 1.1rem;
+  text-align: center;
+}
+
+table thead {
+  background-color: #18283d;
+  color: #d4d4d4;
+}
+
+table thead th {
+  font-weight: bolder;
+}
+
+table tbody tr:nth-child(odd) {
+  background-color: #81848d;
+}
+</style>
